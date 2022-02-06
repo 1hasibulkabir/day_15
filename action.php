9d97bdf34498b16b8e926793023469a5ef54ba3d
unknown <shaownakter.1995@gmail.com>
2022-02-06 09:39:56 +0600

    day_15

---
+++ action.php
@@ -0,0 +1,11 @@
+<?php
+require_once 'vendor/autoload.php';
+use App\classes\Home;
+
+if(isset($_GET['pages']))
+{
+    if($_GET['pages'] == 'home')
+    {
+        include 'pages/home.php';
+    }
+}
